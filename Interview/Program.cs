c66093fd09a28499ad3c941f12a22f7516b09c9e
Thirumeninathan <Thirumeninathan@IDEA-PC>
2017-03-23 08:44:52 -0700

    Add project files.

---
+++ Interview/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interview.RandomNumber;
+
+namespace Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select the Type of Program to Execute:");
+            var randomNumber = new SelectRandomNumber();
+
+            while (true)
+            {
+                var programType = Console.ReadLine();
+                randomNumber.GetInputs();
+                randomNumber.Output();
+            }
+        }
+    }
+}
---
+++ Interview/RandomNumber/SelectRandomNumber.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.RandomNumber
+{
+    public class SelectRandomNumber
+    {
+        public int[] input { get; set; }
+        public int[] frequency { get; set; }
+
+        public void GetInputs()
+        {
+            input = new int[] { 1, 2, 3 };
+            frequency = new int[] { 1, 5, 1 };
+
+        }
+
+        public void Output()
+        {
+            //var test = new Random();
+            //int totalFrequency = 0, rand = 0;
+
+            //for (int i = 0; i < input.Length; i++)
+            //{
+            //    while (totalFrequency < frequency[i])
+            //    {
+            //        totalFrequency += frequency[i];
+            //        rand = test.Next() * (totalFrequency + frequency[i]);
+            //    }
+
+            //    if (rand < totalFrequency)
+            //    {
+            //        Console.WriteLine(input[i]);
+            //        break;
+            //    }
+            //}
+            var maxFrequency = frequency.Max();
+
+            var rand = new Random().Next(0, maxFrequency);
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (frequency[i] >= rand)
+                {
+                    Console.Write(input[i]);
+                    break;
+                }
+                
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
